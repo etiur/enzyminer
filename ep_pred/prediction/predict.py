3cf2501175387a11efc8347b80b170c98671d482
etiur <ruite.xiang@gmail.com>
2022-11-22 15:55:33 +0100

    Change: Change the script to adapt to the final set of models

--- ep_pred/prediction/predict.py
+++ ep_pred/prediction/predict.py
@@ -43,8 +43,8 @@ class EnsembleVoting:
             The path to the directory where the new extracted feature files are
         """
         self.filtered_out = feature_out
-        self.learning = "/gpfs/projects/bsc72/ruite/enzyminer/data/esterase_binary.xlsx"
-        self.models = "/gpfs/projects/bsc72/ruite/enzyminer/models"
+        self.learning = "/gpfs/projects/bsc72/ruite/enzyminer/data/all_feature.xlsx"
+        self.models = "/gpfs/projects/bsc72/ruite/enzyminer/final_models"
 
     def scale_transform(self, file_path, feature_set):
         """
@@ -61,11 +61,6 @@ class EnsembleVoting:
         scaling = MinMaxScaler()
         X_svc = pd.read_excel(f"{self.learning}", index_col=0, sheet_name=f"{feature_set}", engine='openpyxl')
         new_svc = pd.read_csv(f"{file_path}", index_col=0)
-
-        if X_svc.isnull().values.any():
-            X_svc.dropna(axis=1, inplace=True)
-            X_svc.drop(["go"], axis=1, inplace=True)
-
         old_svc = scaling.fit_transform(X_svc)
         transformed_x = scaling.transform(new_svc)
 
@@ -78,27 +73,27 @@ class EnsembleVoting:
         svc = {}
         ridge = {}
         knn = {}
-        # predict random_30 features
-        transformed_x_knn, old_knn = self.scale_transform(f"{self.filtered_out}/knn_features.csv", "random_30")
-        # predict ch2_20 features
-        transformed_x, old_svc = self.scale_transform(f"{self.filtered_out}/svc_features.csv", "ch2_20")
+        # extract the features
+        transformed_x_knn, old_knn = self.scale_transform(f"{self.filtered_out}/knn_features.csv", "xgboost_30")
+        transformed_x_svc, old_svc = self.scale_transform(f"{self.filtered_out}/svc_features.csv", "ch2_30")
+        transformed_x_ridge, old_ridge = self.scale_transform(f"{self.filtered_out}/ridge_features.csv", "random_20")
         # load the saved models
         models = os.listdir(self.models)
         for mod in models:
             if "svc" in mod:
                 mud = joblib.load(f"{self.models}/{mod}")
-                pred = mud.predict(transformed_x)
+                pred = mud.predict(transformed_x_svc)
                 svc[mod.strip(".pkl")] = pred
             elif "ridge" in mod:
                 mud = joblib.load(f"{self.models}/{mod}")
-                pred = mud.predict(transformed_x)
+                pred = mud.predict(transformed_x_ridge)
                 ridge[mod.strip(".pkl")] = pred
             elif "knn" in mod:
                 mud = joblib.load(f"{self.models}/{mod}")
                 pred = mud.predict(transformed_x_knn)
                 knn[mod.replace(".pkl", "")] = pred
 
-        return svc, ridge, knn, transformed_x, old_svc, transformed_x_knn, old_knn
+        return svc, ridge, knn, transformed_x_svc, old_svc, transformed_x_knn, old_knn, transformed_x_ridge, old_ridge
 
     def vote(self, val=1, *args):
         """
@@ -141,8 +136,8 @@ class ApplicabilityDomain():
         self.pred = []
         self.dataframe = None
         self.n_insiders = []
-        path_to_esterase = "/gpfs/projects/bsc72/ruite/enzyminer/data/esterase_binary.xlsx"
-        x_svc = pd.read_excel(f"{path_to_esterase}", index_col=0, sheet_name=f"ch2_20", engine='openpyxl')
+        path_to_esterase = "/gpfs/projects/bsc72/ruite/enzyminer/data/all_feature.xlsx"
+        x_svc = pd.read_excel(f"{path_to_esterase}", index_col=0, sheet_name=f"ch2_30", engine='openpyxl')
         self.training_names = x_svc.index
         self.ad_indices = []
 
@@ -202,9 +197,9 @@ class ApplicabilityDomain():
 
         return self.n_insiders, self.ad_indices
 
-    def _sort_models_vote(self, svc, knn, ridge, filtered_names, min_num=1):
+    def _filter_models_vote(self, svc, knn, ridge, filtered_names, min_num=1):
         """
-        Eliminate predictions of sequences that did not pass the applicability domain threshold
+        Eliminate the models individual predictions of sequences that did not pass the applicability domain threshold
 
         Parameters
         ----------
@@ -214,8 +209,6 @@ class ApplicabilityDomain():
             Predictions from KNNs
         ridge: dict
             Prediction from ridge classifiers
-        idx: list[int]
-            Indices where the votes did not agree
         filtered_names: list[str]
             Names of the test samples after the filtering
         min_num: int
@@ -240,14 +233,20 @@ class ApplicabilityDomain():
 
         return pd.DataFrame(results, index=filtered_names)
 
-    def filter(self, prediction, svc, knn, ridge, min_num=1, path_name="filtered_predictions.parquet"):
+    def filter(self, prediction, svc, knn, ridge, index, min_num=1, path_name="filtered_predictions.parquet"):
         """
-        Filter those predictions that have less than min_num training samples that are within the AD
+        Filter those predictions that have less than min_num training samples
 
         Parameters
         ___________
         prediction: array
             An array of the predictions
+        svc: dict[array]
+            The prediction of the SVC models
+        knn: dict[array]
+            The predictions of the different Knn models
+        ridge: dict[array]
+            The predictions of the different ridge models
         index: array
             The index of those predictions that were not unanimous between different models
         path_name: str, optional
@@ -256,16 +255,18 @@ class ApplicabilityDomain():
             The minimun number of training samples within the AD of the test samples
         """
         # filter the predictions and names  based on the specified number of similar training samples
-        filtered_indices = [d[0] for x, d in enumerate(zip(self.ad_indices, self.n_insiders)) if d[1] >= min_num]
         filtered_pred = [d[0] for x, d in enumerate(zip(prediction, self.n_insiders)) if d[1] >= min_num]
         filtered_names = [d[0] for y, d in enumerate(zip(self.test_names, self.n_insiders)) if d[1] >= min_num]
         filtered_n_insiders = [d for s, d in enumerate(self.n_insiders) if d >= min_num]
-        name_training_sample = pd.Series(filtered_indices, index=filtered_names)
+        filtered_indices = [index.remove(num) for num, x in enumerate(self.n_insiders) if x < min_num and num in index]
+        # Turn the different arrays into pandas Series or dataframes
         pred = pd.Series(filtered_pred, index=filtered_names)
         n_applicability = pd.Series(filtered_n_insiders, index=filtered_names)
-        models = self._sort_models_vote(svc, knn, ridge, filtered_names, min_num)
-        self.pred = pd.concat([pred, n_applicability, name_training_sample, models], axis=1)
-        self.pred.columns = ["prediction", "AD_number", "AD_names"] + list(models.columns)
+        models = self._filter_models_vote(svc, knn, ridge, filtered_names, min_num)
+        agree = [x not in filtered_indices for x in range(len(filtered_names))]
+        # concatenate all the objects
+        self.pred = pd.concat([pred, n_applicability, agree, models], axis=1)
+        self.pred.columns = ["prediction", "AD_number", "agreement"] + list(models.columns)
         self.pred.to_parquet(path_name)
         return self.pred
 
@@ -312,22 +313,32 @@ class ApplicabilityDomain():
 
         return positive, negative
 
-    def find_max_ad(self, pred1, pred2):
+    def find_max_ad(self, pred1, pred2, pred3):
         """
         find the maximum applicability domain of the 2 preds
+        parameters
+        ___________
+        pred1: array
+            svc predictions
+        pred2: array
+            knn predictions
+        pred3: array
+            ridge predictions
         """
-        assert len(pred1) == len(pred2), "Both predictions has different length"
+        assert len(pred1) == len(pred2) == len(pred3), "The predictions have different lengths"
         ad = []
         pred = pred1.copy()
         for idx in pred1.index:
-            if pred1["AD_number"].loc[idx] <= pred2["AD_number"].loc[idx]:
+            if pred3["AD_number"].loc[idx] <= pred1["AD_number"].loc[idx] >= pred2["AD_number"].loc[idx]:
                 ad.append(f'{pred1["AD_number"].loc[idx]}-svc')
-            else:
+            elif pred1["AD_number"].loc[idx] <= pred2["AD_number"].loc[idx] >= pred3["AD_number"].loc[idx]:
                 ad.append(f'{pred2["AD_number"].loc[idx]}-knn')
+            else:
+                ad.append(f'{pred3["AD_number"].loc[idx]}-ridge')
         pred["AD_number"] = ad
         return pred
 
-    def extract(self, fasta_file, pred1=None, pred2=None, positive_fasta="positive.fasta",
+    def extract(self, fasta_file, pred1=None, pred2=None, pred3=None, positive_fasta="positive.fasta",
                 negative_fasta="negative.fasta", res_dir="results"):
         """
         A function to extract those test fasta sequences that passed the filter
@@ -345,8 +356,8 @@ class ApplicabilityDomain():
         res_dir: str, optional
             The folder where to keep the prediction results
         """
-        if pred2 is not None:
-            pred1 = self.find_max_ad(pred1, pred2)
+        if pred2 is not None and pred3 is not None:
+            pred1 = self.find_max_ad(pred1, pred2, pred3)
         positive, negative = self.separate_negative_positive(fasta_file, pred1)
         # writing the positive and negative fasta sequences to different files
         with open(f"{res_dir}/{positive_fasta}", "w") as pos:
@@ -382,14 +393,14 @@ def vote_and_filter(feature_out, fasta_file, min_num=1, res_dir="results", val=1
 
     ensemble = EnsembleVoting(feature_out)
     # predictions
-    svc, ridge, knn, new_svc, X_svc, new_knn, X_knn = ensemble.predicting()
+    svc, ridge, knn, new_svc, X_svc, new_knn, X_knn, new_ridge, X_ridge = ensemble.predicting()
     all_voting, all_index = ensemble.vote(val, *svc.values(), *ridge.values(), *knn.values())
     # applicability domain for scv
     domain_svc = ApplicabilityDomain()
     domain_svc.fit(X_svc)
     domain_svc.predict(new_svc)
     # return the prediction after the applicability domain filter of SVC
-    pred_svc = domain_svc.filter(all_voting, svc, knn, ridge, min_num, f"{res_dir}/svc_domain.parquet")
+    pred_svc = domain_svc.filter(all_voting, svc, knn, ridge, all_index, min_num, f"{res_dir}/svc_domain.parquet")
     domain_svc.extract(fasta_file, pred_svc, positive_fasta=f"positive_svc.fasta",
                        negative_fasta=f"negative_svc.fasta", res_dir=res_dir)
     # applicability domain for KNN
@@ -397,17 +408,27 @@ def vote_and_filter(feature_out, fasta_file, min_num=1, res_dir="results", val=1
     domain_knn.fit(X_knn)
     domain_knn.predict(new_knn)
     # return the prediction after the applicability domain filter of KNN
-    pred_knn = domain_knn.filter(all_voting, svc, knn, ridge, min_num, f"{res_dir}/knn_domain.parquet")
+    pred_knn = domain_knn.filter(all_voting, svc, knn, ridge, all_index, min_num, f"{res_dir}/knn_domain.parquet")
     domain_knn.extract(fasta_file, pred_knn, positive_fasta=f"positive_knn.fasta",
                        negative_fasta=f"negative_knn.fasta", res_dir=res_dir)
-    # Then filter again to see which sequences are within the AD of both algorithms since it is an ensemble classifier
-    name_set = set(pred_svc.index).intersection(set(pred_knn.index))
+
+    # applicability domain for ridge
+    domain_ridge = ApplicabilityDomain()
+    domain_ridge.fit(X_knn)
+    domain_ridge.predict(new_knn)
+    # return the prediction after the applicability domain filter of KNN
+    pred_ridge = domain_knn.filter(all_voting, svc, knn, ridge, all_index, min_num, f"{res_dir}/ridge_domain.parquet")
+    domain_ridge.extract(fasta_file, pred_ridge, positive_fasta=f"positive_ridge.fasta",
+                       negative_fasta=f"negative_ridge.fasta", res_dir=res_dir)
+    # Then filter again to see which sequences are within the AD of the 3 algorithms since it is an ensemble classifier
+    name_set = set(pred_svc.index).intersection(set(pred_knn.index), set(pred_ridge.index))
     name_set = sorted(name_set, key=lambda x: int(x.split("_")[1]))
     knn_set = pred_knn.loc[name_set]
+    ridge_set = pred_ridge.loc[name_set]
     common_domain = pred_svc.loc[name_set]
     common_domain.to_parquet(f"{res_dir}/common_domain.parquet")
     # the positive sequences extracted will have the AD of the SVC
-    domain_knn.extract(fasta_file, common_domain, knn_set, res_dir=res_dir)
+    domain_knn.extract(fasta_file, common_domain, knn_set, ridge_set,res_dir=res_dir)
 
 
 def main():
